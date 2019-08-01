c86916f6ab5839172f57e9211fb13474bfb296b5
Mickey Smith <mickey@mickmac.local>
2019-07-31 20:26:15 -0500

    Began directory for practicing algorithms with OpenSSL library for encryption

---
+++ Crypto/AES/aes_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
+int main(int argc, char ** argv) {
+
+    printf("Beginning execution of program: %s\n", argv[0]);
+
+    return EXIT_SUCCESS;
+}
