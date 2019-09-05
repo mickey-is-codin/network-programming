1321fd5e00d5d63bcc50776ad997ea624b16dfa0
Mickey Smith <smithmick14@gmail.com>
2019-09-04 20:51:19 -0500

    Working on basic node and edge creation in networkx_test.py

--- graph-theory/networkx/networkx_test.py
+++ graph-theory/networkx/networkx_test.py
@@ -4,7 +4,31 @@ def main():
 
     print('Beginning program...')
 
+    graph = nx.Graph()
+    print(type(graph))
 
+    graph.add_node('A')
+    graph.add_node('B')
+    graph.add_node('C')
+    graph.add_node('D')
+    graph.add_node('E')
+
+    graph.add_edge('A', 'B')
+    graph.add_edge('A', 'C')
+    graph.add_edge('A', 'D')
+
+    graph.add_edge('B', 'C')
+    graph.add_edge('B', 'D')
+
+    graph.add_edge('D', 'E')
+
+    print('\nNodes in graph: ')
+    for node in graph:
+        print(node)
+
+    print('\nEdges in graph: ')
+    for edge_pair in graph.edges:
+        print(edge_pair)
 
 if __name__ == '__main__':
     main()
